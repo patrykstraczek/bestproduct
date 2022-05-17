fc0f1e5a9b0bcc33109ab2eda7c51c466cde8486
Patryk Strączek <patrykstraczek99@gmail.com>
2022-05-17 14:25:52 +0200

    Add registration

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:bestproduct/app/login/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
