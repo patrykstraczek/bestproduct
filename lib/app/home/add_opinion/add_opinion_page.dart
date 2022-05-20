14b8207b8338e34b792f88354e765a665db3f6c0
Patryk Strączek <patrykstraczek99@gmail.com>
2022-05-20 13:56:22 +0200

    Implement adding restaurants

--- lib/app/home/add_opinion/add_opinion_page.dart
+++ lib/app/home/add_opinion/add_opinion_page.dart
@@ -1,18 +1,57 @@
-import 'package:bestproduct/app/home/my_account/my_account_page.dart';
-import 'package:bestproduct/app/home/restaurants/restaurants_page_content.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPage extends StatelessWidget {
+class AddOpinionPage extends StatefulWidget {
   const AddOpinionPage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPage> createState() => _AddOpinionPageState();
+}
+
+class _AddOpinionPageState extends State<AddOpinionPage> {
+  var restaurantName = '';
+  var pizzaName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: ('Podaj nazwę restauracji'),
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: ('Podaj nazwę pizzy'),
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                pizzaName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantName,
+                'pizza': pizzaName,
+                'rating': 3.0,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
