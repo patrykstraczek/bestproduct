ace12aac34f4a66cc08bcc5932e0b8c6dad30fb2
Patryk Strączek <patrykstraczek99@gmail.com>
2022-06-10 12:16:34 +0200

    finish section 6

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,8 @@
+import 'package:bestproduct/app/cubit/root_cubit.dart';
 import 'package:bestproduct/app/features/home/home_page.dart';
 import 'package:bestproduct/app/features/login/login_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -25,15 +26,17 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        final user = snapshot.data;
-        if (user == null) {
-          return LoginPage();
-        }
-        return HomePage(user: user);
-      },
+    return BlocProvider(
+      create: (context) => RootCubit()..start(),
+      child: BlocBuilder<RootCubit, RootState>(
+        builder: (context, state) {
+          final user = state.user;
+          if (user == null) {
+            return LoginPage();
+          }
+          return HomePage(user: user);
+        },
+      ),
     );
   }
 }
