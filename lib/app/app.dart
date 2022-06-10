81e2c4ede3d35e968a68a6f969e13f9e8b0d6be0
Patryk Strączek <patrykstraczek99@gmail.com>
2022-06-10 10:58:38 +0200

    move myApp tp app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,39 @@
+import 'package:bestproduct/app/features/home/home_page.dart';
+import 'package:bestproduct/app/features/login/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        final user = snapshot.data;
+        if (user == null) {
+          return LoginPage();
+        }
+        return HomePage(user: user);
+      },
+    );
+  }
+}
